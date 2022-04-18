dd967f2bcc9db6e4264cff12d7f1f69276f07208
Karsyz <mattkars@gmail.com>
2022-04-18 14:58:32 -0400

    added functions.js and updated index.html

---
+++ JS/functions/functions.js
@@ -0,0 +1,28 @@
+function getReminder() {
+    console.log('Water the plants.')
+  }
+  
+  function greetInSpanish() {
+    console.log("Buenas Tardes.")
+  }
+
+
+function sayThanks() {
+    console.log('Thank you for your purchase! We appreciate your business.')
+}
+
+// function sayThanks(a) {
+//     let i = 0
+//     while (i < a) {
+//         console.log('Thank you for your purchase! We appreciate your business.')
+//         i++
+//     }
+
+// }
+
+sayThanks()
+sayThanks()
+sayThanks()
+
+
+
