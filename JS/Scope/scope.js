3b95a53fce80d9449da247fb9e2c6272fb9f4cab
Karsyz <mattkars@gmail.com>
2022-04-19 22:31:41 -0400

    added scope.js

---
+++ JS/Scope/scope.js
@@ -0,0 +1,52 @@
+const city = 'New York City'
+
+function logCitySkyline() {
+  let skyscraper = 'Empire State Building'
+
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+
+console.log(logCitySkyline())
+
+
+let satellite = 'The Moon'
+
+let galaxy = 'The Milky Way'
+
+let stars = 'North Star'
+
+function callMyNightSky () {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log (callMyNightSky())
+
+
+
+
+function logVisibleLightWaves() {
+    const lightWaves = 'Moonlight'
+    console.log(lightWaves)
+  }
+  
+  logVisibleLightWaves()
+  
+  console.log(lightWaves)  //  throws undefined error
+
+
+
+
+
+  const satellite = 'The Moon';
+  const galaxy = 'The Milky Way';
+  let stars = 'North Star';
+  
+  const callMyNightSky = () => {
+    stars = 'Sirius';  //  redefines stars
+      return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+  };
+  
+  console.log(callMyNightSky());
+  console.log(stars);  // shows redefined 
+  
+
