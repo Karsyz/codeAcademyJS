99b2672e4433227518dc53816c98d9a97f9d0f38
Karsyz <mattkars@gmail.com>
2022-05-01 22:41:02 -0400

    changes

---
+++ JS/arrays/arrays.js
@@ -0,0 +1,29 @@
+const hobbies = ["asdf","asdfg","asdfgh"]
+console.log(hobbies)
+
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0]
+console.log(listItem)
+console.log(famousSayings[2])
+console.log(famousSayings[3])
+
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados'
+
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo'
+console.log(condiments)
+
+condiments = ['Mayo']
+
+utensils[3] = 'Spoon'
+console.log(utensils)
+
+
